f2875cfbbcef892dac46d26a7c76589a1d7b8c29
nrsabit <mailbox.sabit@gmail.com>
2023-05-03 21:52:54 +0600

    api for single recipe added

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const port = 5000;
 const cors = require('cors');
 const chefs = require('./chef-data/chef-data.json');
+const recipes = require('./chef-data/recipes.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Bangla Foods is Runninggg')
+    res.send('Bangla Foods is Running')
 });
 
 app.get('/chefs', (req, res) => {
@@ -16,12 +17,15 @@ app.get('/chefs', (req, res) => {
 
 app.get('/chefs/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    console.log(id)
     const selectedChef = chefs.find(chef => chef.id === id);
-    console.log(selectedChef)
     res.send(selectedChef)
 })
 
+app.get('/recipe/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedRecipe = recipes.find(recipe => recipe.id === id);
+    res.send(selectedRecipe)
+})
 
 app.listen(port, () => {
     console.log(`Here is the Bangla Foods Server ${port}`)
