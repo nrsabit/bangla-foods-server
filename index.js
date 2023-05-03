9d639453c92fbdd409fbf915b1964163bcc12ded
nrsabit <mailbox.sabit@gmail.com>
2023-05-03 20:49:02 +0600

    api for single chef added

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors');
 const chefs = require('./chef-data/chef-data.json');
 
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Bangla Foods is Running')
+    res.send('Bangla Foods is Runninggg')
 });
 
 app.get('/chefs', (req, res) => {
     res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id)
+    const selectedChef = chefs.find(chef => chef.id === id);
+    console.log(selectedChef)
+    res.send(selectedChef)
 })
 
 
